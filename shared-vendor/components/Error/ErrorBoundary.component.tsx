8c99722bce4733f056fb797ae5d779d83a25d96a
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-30 20:12:54 +0330

    feat: Add ErrorBoundary

---
+++ shared-vendor/components/Error/ErrorBoundary.component.tsx
@@ -0,0 +1,25 @@
+import { type FallbackProps, ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
+
+import ErrorSvg from "@shared-vendor/assets/svg/error.svg?react";
+
+const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
+  const message: string = error?.message ?? error;
+
+  return (
+    <div className="app-container flex size-full items-center justify-center">
+      <Card className="flex flex-col items-center gap-8 p-8">
+        <ErrorSvg className="size-60" />
+
+        <span className="text-xl font-extrabold text-red-400">مشکلی پیش آمده است...</span>
+
+        <span className="font-semibold text-red-300">{message}</span>
+
+        <BaseButton label="تلاش دوباره" className="w-80" onClick={resetErrorBoundary} />
+      </Card>
+    </div>
+  );
+};
+
+export const ErrorBoundary = ({ children }: FragmentProps) => {
+  return <ReactErrorBoundary FallbackComponent={ErrorFallback}>{children}</ReactErrorBoundary>;
+};
