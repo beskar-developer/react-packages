2da196990ecc8b8d05cc16b341d01682189db94c
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-05 09:17:02 +0330

    feat: Add variants to BaseButton

--- shared-vendor/components/Button/BaseButton.component.tsx
+++ shared-vendor/components/Button/BaseButton.component.tsx
@@ -1,6 +1,19 @@
-import type { Props } from "./BaseButton.type";
+import type { Props, Variant } from "./BaseButton.type";
 
-export const BaseButton = ({ children, className, disabled, loading, ...props }: Props) => {
+const BUTTON_CLASS_MAP: Record<Variant, string> = {
+  filled: "bg-indigo-500 text-white ",
+  tonal: "bg-indigo-200 text-indigo-500 ",
+  outlined: "bg-transparent border-2 border-indigo-500 text-indigo-500",
+};
+
+export const BaseButton = ({
+  children,
+  className,
+  disabled,
+  loading,
+  variant = "outlined",
+  ...props
+}: Props) => {
   const { onClick } = useBaseButton({
     disabled,
     loading,
@@ -12,7 +25,8 @@ export const BaseButton = ({ children, className, disabled, loading, ...props }:
       onClick={onClick}
       disabled={disabled}
       className={twMerge(
-        "flex h-10 cursor-pointer items-center justify-center rounded-md bg-indigo-500 p-2 text-center text-sm text-white transition select-none hover:scale-[105%] hover:bg-indigo-400 disabled:cursor-not-allowed disabled:opacity-55",
+        "flex h-10 cursor-pointer items-center justify-center rounded-md p-2 text-center text-sm transition select-none hover:scale-[105%] disabled:cursor-not-allowed disabled:opacity-55",
+        BUTTON_CLASS_MAP[variant],
         className,
       )}
       {...props}
--- shared-vendor/components/Button/BaseButton.type.ts
+++ shared-vendor/components/Button/BaseButton.type.ts
@@ -1,4 +1,7 @@
+export type Variant = "filled" | "tonal" | "outlined";
+
 export interface Props extends ComponentProps<"button"> {
   label?: string;
   loading?: boolean;
+  variant?: Variant;
 }
