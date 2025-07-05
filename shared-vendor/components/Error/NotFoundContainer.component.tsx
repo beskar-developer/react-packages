4cd6f394cc06e17dd2047e753c919a4f6ce933e7
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-05 19:19:19 +0330

    feat: Add NotFound container

---
+++ shared-vendor/components/Error/NotFoundContainer.component.tsx
@@ -0,0 +1,15 @@
+import { itemAnimationConfig } from "@shared-vendor/constants";
+
+interface Props {
+  children: ReactNode;
+  itemCount: number;
+  message: string;
+}
+
+export const NotFoundContainer = ({ children, message, itemCount }: Props) => {
+  return (
+    <AnimatePresence>
+      {!itemCount ? <NotFound message={message} {...itemAnimationConfig} /> : children}
+    </AnimatePresence>
+  );
+};
