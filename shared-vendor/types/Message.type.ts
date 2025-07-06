16e92f5c83f46b381d5c0d8299c5c1913ed98a5d
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-06 12:19:05 +0330

    feat: Add success message support to Message decorator

--- shared-vendor/types/Message.type.ts
+++ shared-vendor/types/Message.type.ts
@@ -1,15 +1,23 @@
 /* eslint-disable func-style */
-export const ErrorMessage = (errorMessage: string, showToast: boolean = true) =>
+type Options = Partial<{
+  error: string;
+  success: string;
+  showToast: boolean;
+}>;
+
+export const Message = ({ error: errorMessage, success: successMessage, showToast = true }: Options = {}) =>
   function <This, Args extends unknown[], Return>(target: (this: This, ...args: Args) => Promise<Return>) {
     async function replacementMethod(this: This, ...args: Args): Promise<Return> {
       try {
         const result = await target.call(this, ...args);
 
+        if (showToast && successMessage) toast.success(successMessage);
+
         return result;
       } catch (error) {
         console.error(error);
 
-        if (showToast) toast.error(errorMessage);
+        if (showToast && errorMessage) toast.error(errorMessage);
 
         throw error;
       }
--- shared-vendor/types/index.ts
+++ shared-vendor/types/index.ts
@@ -1,4 +1,4 @@
 export * from "./ThemeMode.type";
 export * from "./Storage.type";
 export * from "./HttpClient.type";
-export * from "./Error.type";
+export * from "./Message.type";
