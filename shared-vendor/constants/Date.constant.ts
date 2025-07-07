45bdbe1a3afa157456d9f86f560885e5431bcdc5
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-07 15:10:58 +0330

    feat: Add Date constant

---
+++ shared-vendor/constants/Date.constant.ts
@@ -0,0 +1,1 @@
+export const FULL_DATE_FORMAT = "yyyy/MM/d";
--- shared-vendor/constants/index.ts
+++ shared-vendor/constants/index.ts
@@ -1,3 +1,4 @@
 export { default as MEDIA_QUERY } from "./MediaQuery.constant";
 export * from "./ErrorMessage.constant";
 export * from "./Motion.constant";
+export * from "./Date.constant";
