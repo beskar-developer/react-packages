e39e05fa7285d06f37914aee2b646dce8bf23f35
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-12 16:08:05 +0330

    feat: Add sky color to BaseButton

--- shared-vendor/components/Button/BaseButton.component.tsx
+++ shared-vendor/components/Button/BaseButton.component.tsx
@@ -7,24 +7,28 @@ const BUTTON_CLASS_MAP: Record<Variant, Record<Color, string>> = {
     red: "bg-red-500 text-red-50",
     neutral: "bg-black text-white dark:bg-white dark:text-black",
     emerald: "bg-emerald-500 text-emerald-50",
+    sky: "bg-sky-500 text-sky-50",
   },
   tonal: {
     indigo: "bg-indigo-200 text-indigo-500",
     red: "bg-red-200 text-red-500",
     neutral: "",
     emerald: "bg-emerald-200 text-emerald-500",
+    sky: "bg-sky-200 text-sky-500",
   },
   outlined: {
     indigo: twMerge(OUTLINED_BASE_CLASS, "border-indigo-500 text-indigo-500"),
     red: twMerge(OUTLINED_BASE_CLASS, "border-red-500 text-red-500"),
     neutral: OUTLINED_BASE_CLASS,
     emerald: twMerge(OUTLINED_BASE_CLASS, "border-emerald-500 text-emerald-500"),
+    sky: twMerge(OUTLINED_BASE_CLASS, "border-sky-500 text-sky-500"),
   },
   text: {
     indigo: "hover:bg-indigo-200 hover:text-indigo-600 text-indigo-500",
     red: "hover:bg-red-200 hover:text-red-600 text-red-500",
     neutral: "",
     emerald: "hover:bg-emerald-200 hover:text-emerald-600 text-emerald-500",
+    sky: "hover:bg-sky-200 hover:text-sky-600 text-sky-500",
   },
 };
 
--- shared-vendor/components/Button/BaseButton.type.ts
+++ shared-vendor/components/Button/BaseButton.type.ts
@@ -1,5 +1,5 @@
 export type Variant = "filled" | "tonal" | "outlined" | "text";
-export type Color = "indigo" | "red" | "emerald" | "neutral";
+export type Color = "indigo" | "red" | "emerald" | "neutral" | "sky";
 
 export interface Props extends ComponentProps<"button"> {
   label?: string;
