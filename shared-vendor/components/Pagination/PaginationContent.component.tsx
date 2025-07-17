1b7f698b42959ce01b7b094c770abff31596fb1a
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-07-17 12:38:29 +0330

    fix: Fix pagination style

--- shared-vendor/components/Pagination/PaginationContent.component.tsx
+++ shared-vendor/components/Pagination/PaginationContent.component.tsx
@@ -2,7 +2,7 @@ import type { IPagination } from "./Pagination.type";
 
 export const PaginationContent = ({ totalPages, page }: Pick<IPagination, "totalPages" | "page">) => {
   return (
-    <div className="flex min-w-10 shrink-0 items-center justify-center">
+    <div className="flex min-w-10 shrink-0 items-center justify-center sm:min-w-20">
       <span className="text-surface-600 dark:text-surface-300 text-xs">{totalPages}</span> / {page}
     </div>
   );
